52f46cdba0bdd585b2075a811bd389712dbb17cd
Vern Kofford <cheow3@gmail.com>
2017-12-11 15:04:02 -0700

    Initial build for proposal form

--- freelance-camp-fe/app/app.module.ts
+++ freelance-camp-fe/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent} from './homepage/homepage.component';
@@ -11,7 +12,8 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        FormsModule
     ],
     declarations: [
         AppComponent,
