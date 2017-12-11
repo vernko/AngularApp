5ebde1899df4755d8759e0834da0eff16ac68f34
Vern Kofford <cheow3@gmail.com>
2017-12-11 12:40:45 -0700

    Implemented mock data for docs

--- freelance-camp-fe/app/documents/documents.component.js
+++ freelance-camp-fe/app/documents/documents.component.js
@@ -9,6 +9,29 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var DocumentsComponent = /** @class */ (function () {
     function DocumentsComponent() {
+        this.documents = [
+            {
+                title: "My First Doc",
+                description: 'asdf lkajsdflk',
+                file_url: 'http://google.com',
+                updated_at: '12/08/17',
+                image_url: 'http://google.com',
+            },
+            {
+                title: "My Second Doc",
+                description: 'asdf lkajsdflk',
+                file_url: 'http://google.com',
+                updated_at: '12/08/17',
+                image_url: 'http://google.com',
+            },
+            {
+                title: "My Last Doc",
+                description: 'asdf lkajsdflk',
+                file_url: 'http://google.com',
+                updated_at: '12/08/17',
+                image_url: 'http://google.com',
+            }
+        ];
     }
     DocumentsComponent = __decorate([
         core_1.Component({
--- freelance-camp-fe/app/documents/documents.component.ts
+++ freelance-camp-fe/app/documents/documents.component.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import { Document } from './document';
 
-@Component ({
+@Component({
     moduleId: module.id,
     selector: 'documents',
     templateUrl: 'documents.component.html'
 })
-export class DocumentsComponent {}
+export class DocumentsComponent {
+    documents: Document[] = [
+        {
+            title: "My First Doc",
+            description: 'asdf lkajsdflk',
+            file_url: 'http://google.com',
+            updated_at: '12/08/17',
+            image_url: 'http://google.com',
+        },
+        {
+            title: "My Second Doc",
+            description: 'asdf lkajsdflk',
+            file_url: 'http://google.com',
+            updated_at: '12/08/17',
+            image_url: 'http://google.com',
+        },
+        {
+            title: "My Last Doc",
+            description: 'asdf lkajsdflk',
+            file_url: 'http://google.com',
+            updated_at: '12/08/17',
+            image_url: 'http://google.com',
+        }
+    ]
+}
