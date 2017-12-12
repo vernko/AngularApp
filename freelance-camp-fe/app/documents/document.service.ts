cc6e06e2d1495ab94e052d3a8fa6c5131b04eb09
Vern Kofford <cheow3@gmail.com>
2017-12-12 14:46:24 -0700

    Imported Observable to use for docs

--- freelance-camp-fe/app/documents/document.service.ts
+++ freelance-camp-fe/app/documents/document.service.ts
@@ -1,6 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+
+import { Document } from './document';
 
 @Injectable()
 export class DocumentService {
-    
+    private documentsUrl = 'http://localhost:3001/freelance_documents.json';
+
+    constructor(
+        private http: Http
+    ){}
+
+    getDocuments(): Observable<Document[]> {
+        return this.http.get(this.documentsUrl)
+                        .map((response: Response) => <Document[]>response.json())
+    }
 }
