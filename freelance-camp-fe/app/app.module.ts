230a8186d0ce48dd8df8fa6aa57d2d7febae0da8
Vern Kofford <cheow3@gmail.com>
2017-12-11 18:22:48 -0700

    Created show component for proposals, plus route

--- freelance-camp-fe/app/app.module.ts
+++ freelance-camp-fe/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomepageComponent} from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { ProposalListComponent } from './proposal/proposal-list.component';
 import { ProposalNewComponent } from './proposal/proposal-new.component';
+import { ProposalShowComponent } from './proposal/proposal-show.component';
 
 import { AppRoutingModule } from './app-routing.module';
 @NgModule({
@@ -20,7 +21,8 @@ import { AppRoutingModule } from './app-routing.module';
         HomepageComponent,
         DocumentsComponent,
         ProposalListComponent,
-        ProposalNewComponent
+        ProposalNewComponent,
+        ProposalShowComponent
     ],
     bootstrap: [
         AppComponent
