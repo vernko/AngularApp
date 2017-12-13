90535971c66dbc60a95d9688f5f93c47de0b4897
Vern Kofford <cheow3@gmail.com>
2017-12-12 18:00:46 -0700

    Implemented styles for proposals list with live data

--- freelance-camp-fe/app/app.module.ts
+++ freelance-camp-fe/app/app.module.ts
@@ -11,6 +11,7 @@ import { DocumentService } from './documents/document.service';
 import { ProposalListComponent } from './proposal/proposal-list.component';
 import { ProposalNewComponent } from './proposal/proposal-new.component';
 import { ProposalShowComponent } from './proposal/proposal-show.component';
+import { ProposalService } from './proposal/proposal.service';
 
 import { AppRoutingModule } from './app-routing.module';
 @NgModule({
@@ -30,7 +31,8 @@ import { AppRoutingModule } from './app-routing.module';
         ProposalShowComponent
     ],
     providers: [
-        DocumentService
+        DocumentService,
+        ProposalService
     ],
     bootstrap: [
         AppComponent
--- freelance-camp-fe/app/proposal/proposal-list.component.ts
+++ freelance-camp-fe/app/proposal/proposal-list.component.ts
@@ -1,20 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Proposal } from './proposal';
+import { ProposalService } from './proposal.service';
 
-@Component ({
+@Component({
     moduleId: module.id,
     selector: 'proposal-list',
     templateUrl: 'proposal-list.component.html',
-    styleUrls: ['proposal-list.component.css']
+    styleUrls: ['proposal-list.component.css'],
+    providers: [ ProposalService ]
 })
-export class ProposalListComponent {
-    proposalOne: Proposal = new Proposal(15, 'ABC Company', 'http://portoflio.vernkofford.com', 'Ruby on Rails', 150, 120, 15, 'vern.kofford@gmail.com')
-    proposalTwo: Proposal = new Proposal(3, 'XYZ Company', 'http://portoflio.vernkofford.com', 'Ruby on Rails', 150, 120, 15, 'vern.kofford@gmail.com')
-    proposalThree: Proposal = new Proposal(300, 'That Company', 'http://portoflio.vernkofford.com', 'Ruby on Rails', 150, 120, 15, 'vern.kofford@gmail.com')
+export class ProposalListComponent implements OnInit {
+    proposals: Proposal[];
+    errorMessage: string;
+    mode = "Observable";
 
-    proposals: Proposal[] = [
-        this.proposalOne,
-        this.proposalTwo,
-        this.proposalThree
-    ]
+    constructor(
+        private proposalService: ProposalService
+    ) {}
+
+    ngOnInit() {
+        let timer = Observable.timer(0, 5000);
+        timer.subscribe(() => this.getProposals());
+    }
+
+    getProposals() {
+        this.proposalService.getProposals()
+            .subscribe(
+                proposals => this.proposals = proposals,
+                error => this.errorMessage = <any>error
+            );
+    }
 }
